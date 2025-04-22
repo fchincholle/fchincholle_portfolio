1c4e6a7096588d3f739913a5a63923aa83f522d3
fchincholle <franckchincholle12@gmail.com>
2025-04-22 15:18:33 +0200

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,6 @@ import PdfViewer from '../components/PdfViewer';
 import { pdfjs } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
     import.meta.url,
   ).toString();
 
