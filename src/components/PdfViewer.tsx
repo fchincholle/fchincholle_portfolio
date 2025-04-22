38750f7be0bc33acfc8d1484cdd07519025e1676
fchincholle <franckchincholle12@gmail.com>
2025-04-22 13:40:29 +0200

    test pdf

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
 import { pdfjs } from 'react-pdf';
+import { Document, Page } from 'react-pdf';
+import { useState } from 'react';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import workerSrc from 'pdfjs-dist/build/pdf.worker.entry';
 
-const PdfViewer: React.FC = () => {
-  const [pdfUrl] = useState<string>('/Franck_Chincholle_CV.pdf');
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+interface PdfViewerProps {
+  file: string;
+}
+
+const PdfViewer: React.FC<PdfViewerProps> = ({ file }) => {
+  const [numPages, setNumPages] = useState<number | null>(null);
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setNumPages(numPages);
+  };
 
   return (
     <div>
-      <h2>Visualisation du document</h2>
-      <div style={{ height: '750px', border: '1px solid #ccc' }}>
-        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}>
-          <Viewer fileUrl={pdfUrl} />
-        </Worker>
-      </div>
+      <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page key={index + 1} pageNumber={index + 1} />
+        ))}
+      </Document>
     </div>
   );
 };
